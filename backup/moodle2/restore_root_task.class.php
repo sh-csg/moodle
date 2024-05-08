b65a9bc9965897667291b3414c95b536f95d3068
Stefan Hanauska <stefan.hanauska@csg-in.de>
2024-05-08 12:22:44 +0200

    MDL-81511 backup: Change checksum calculation

--- backup/moodle2/restore_root_task.class.php
+++ backup/moodle2/restore_root_task.class.php
@@ -35,7 +35,7 @@ use core_backup\hook\after_restore_root_define_settings;
  *
  * TODO: Finish phpdocs
  */
-class restore_root_task extends restore_task {
+class restore_root_task extends restore_task implements checksumable {
 
     /**
      * Create all the steps that will be part of this task
@@ -351,5 +351,24 @@ class restore_root_task extends restore_task {
         // Create and dispatch a hook to allow plugins to add other settings for the restore process.
         $hook = new after_restore_root_define_settings($this);
         di::get(manager::class)->dispatch($hook);
+    }
+
+    /**
+     * Test if a given checksum is correct for this task.
+     *
+     * @param string $checksum
+     * @return boolean True if the checksum is correct, false otherwise.
+     */
+    public function is_checksum_correct($checksum) {
+        return $this->calculate_checksum() === $checksum;
+    }
+
+    /**
+     * Calculate checksum for this task.
+     *
+     * @return string Checksum as MD5 hash.
+     */
+    public function calculate_checksum() {
+        return md5($this->name . '-' . backup_general_helper::array_checksum_recursive($this->settings));
     }
 }
